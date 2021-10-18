b71226e125cfd448687663d7218926594318b782
kitanoyoru <ale_rutk@yahoo.com>
2021-10-18 22:27:24 +0300

    update some things

--- Lab1/Lab1.cpp
+++ Lab1/Lab1.cpp
@@ -2,7 +2,7 @@
 
 #include <iostream>
 #include <vector>
-#include <string>
+#include <algorithm>
 using namespace std;
 
 /*==========[Class]==========*/
@@ -15,25 +15,59 @@ class Set {
         Set (vector<int> s1, vector<int> s2): set1(s1), set2(s2) {}
 
         void intersection();
-        vector<int> unity();
+        void unity();
 };
 
 /*==========[Class Functions]==========*/
 // need to repair
-vector<int> Set::unity() {
+void Set::unity() {
     vector<int> ans;
+    if (set1.size() == 0 && set2.size() == 0) {
+        cout << "Пустое множество!\n";
+        cout << "Результат: {}";
+        return;
+    }
+    if (set1.size() == 0) {
+        cout << "Результат: {";
+        for (int i = 0; i < set2.size(); i++) {
+            if (i != set2.size()-1)
+                cout << set2[i] << ", ";
+            else
+                cout << set2[i] << "}";
+        }
+        return;
+    }
+    if (set2.size() == 0) {
+        cout << "Результат: {";
+        for (int i = 0; i < set1.size(); i++) {
+            if (i != set1.size()-1)
+                cout << set1[i] << ", ";
+            else
+                cout << set1[i] << "}";
+        }
+        return;      
+    }
     for (auto it: set1) {ans.push_back(it);}
-    for (auto i: set1)
-        for (auto j: set2)
-            if (i == j) {
-                continue;
-            }
-            else {
-                ans.push_back(j);
-            }
-    return ans;
+    vector<int> positions;
+    for (int i = 0; i < set1.size(); i++)
+        for (int j = 0; j < set2.size(); j++)
+            if (set1[i] == set2[j])
+                positions.push_back(j);
+    if (positions.size() != 0)
+        for (int i = 0; i < set2.size(); i++) {
+            if (*find(positions.begin(), positions.end(), i) != i) 
+                ans.push_back(set2[i]);
+        }
+
+    cout << "\nРезультат: {";
+    for (int i = 0; i < ans.size(); i++) {
+        if (i != ans.size()-1)
+            cout << ans[i] << ", ";
+        else
+            cout << ans[i] << "}";
+    }
 }
-// work
+
 void Set::intersection() {
     vector<int> ans;
     for (auto i: set1)
@@ -54,26 +88,46 @@ void Set::intersection() {
 
 int main () 
 {
-    vector<int> s1;
-    vector<int> s2;
     int number;
-    int power_s1, power_s2;
 
+    int power_s1; 
     cout << "Введите мощность множества А: "; cin >> power_s1;
+    
+Problem_s1:    
+    vector<int> s1;
     cout << "Введите множество А: ";
     for (int i = 0; i < power_s1; i++) {cin >> number; s1.push_back(number);}
+    for (int i = 0; i < power_s1; i++)
+        for (int j = 0; j < power_s1; j++)
+            if (i != j)
+                if (s1[i] == s1[j]) {
+                    cout << "Ошибка! Введите множество заново!\n";
+                    goto Problem_s1;
+                }
+
+    int power_s2;
     cout << endl;
     cout << "Введите мощность множества B: "; cin >> power_s2;
+
+Problem_s2:
+    vector<int> s2;
     cout << "Введите множество B: ";
     for (int i = 0; i < power_s2; i++) {cin >> number; s2.push_back(number);}
+    for (int i = 0; i < power_s2; i++)
+        for (int j = 0; j < power_s2; j++)
+            if (i != j)
+                if (s2[i] == s2[j]) {
+                    cout << "Ошибка! Введите множество заново!\n";
+                    goto Problem_s2;
+                }
 
     Set s(s1, s2);
-    s.intersection();
+    s.unity();
 
     return 0;
 
 }
 
-/* 1.Починить unity
+/* 
    2. Если мошность введенного множества меньше power;
    3. Сделать выбор функции в main */
